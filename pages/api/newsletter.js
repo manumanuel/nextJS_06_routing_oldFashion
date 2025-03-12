329ebf621044f0eb0e2ea67a4a2a0ae70ef079fe
Manu Manuel <manuelmanu008@gmail.com>
2025-03-12 07:02:17 +0530

    adding mongodb setup

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,3 +1,5 @@
+import { MongoClient } from "mongodb";
+
 function EmailHandler(req, res) {
   if (req.method === "POST") {
     const userEmail = req.body.email;
@@ -5,7 +7,10 @@ function EmailHandler(req, res) {
       res.status(422).json({ message: "invalid format" });
       return;
     }
+    MongoClient.connect('mongodb+srv://manuelmanu008:<db_password>@cluster0.6nblf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+
     res.status(201).json({ message: "email registered" });
+    
   }
 }
 export default EmailHandler;
